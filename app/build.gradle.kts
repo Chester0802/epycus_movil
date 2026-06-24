2775b611fab7250c220d7d5e49e18ac4b0ae5436
Soltectos <contacto@soltectos.com>
2026-06-24 12:59:32 -0500

    Fix: launcher icon con logo Epycus, API URL apunta a VPS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
 
     buildTypes {
         debug {
-            buildConfigField("String", "API_BASE_URL", "\"http://10.0.2.2:5053/\"")
+            buildConfigField("String", "API_BASE_URL", "\"https://app.epycus.es/\"")
         }
         release {
             isMinifyEnabled = true
